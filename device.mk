ebde9dca9faaa78b7cc08cc7fff21a486e27acfb
R. Andrew Ohana <andrew.ohana@gmail.com>
2012-08-18 15:45:38 -0700

    move resources to common repo

--- device.mk
+++ device.mk
@@ -18,8 +18,3 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/samsung/smdk4210-tab/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/p6810/vendor.mk)
-
-# the Tab 7.7 has a xlarge hpdi screen
-PRODUCT_AAPT_CONFIG := xlarge hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-PRODUCT_LOCALES += hdpi
